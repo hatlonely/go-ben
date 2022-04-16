a50a9bf39c979bb61a6db41d94bd1da2ec7e9124
hatlonely <hatlonely@gmail.com>
2022-04-17 01:14:20 +0800

    update

--- internal/monitor/monitor_psutil.go
+++ internal/monitor/monitor_psutil.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"time"
 
-	"github.com/shirou/gopsutil/v3/net"
-
-	"github.com/shirou/gopsutil/v3/disk"
-
-	"github.com/shirou/gopsutil/v3/mem"
-
 	"github.com/pkg/errors"
 	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/net"
 
 	"github.com/hatlonely/go-ben/internal/stat"
 )
