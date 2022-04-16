eb2e769561effdc45f777c840bb2e7d4058eb444
hatlonely <hatlonely@gmail.com>
2022-04-16 20:20:00 +0800

    update

--- internal/stat/plan_stat.go
+++ internal/stat/plan_stat.go
@@ -1,1 +1,26 @@
 package stat
+
+type PlanStat struct {
+	ID         string
+	Name       string
+	IsErr      bool
+	Err        string
+	UnitGroups []*UnitGroupStat
+}
+
+func NewPlanStat(id string, name string) *PlanStat {
+	return &PlanStat{
+		ID:   id,
+		Name: name,
+	}
+}
+
+func (s *PlanStat) SetError(err error) *PlanStat {
+	s.IsErr = true
+	s.Err = err.Error()
+	return s
+}
+
+func (s *PlanStat) AddUnitGroupStat(unitGroup *UnitGroupStat) {
+	s.UnitGroups = append(s.UnitGroups, unitGroup)
+}
--- internal/stat/test_stat.go
+++ internal/stat/test_stat.go
@@ -5,7 +5,31 @@ type TestStat struct {
 	Directory   string
 	Name        string
 	Description string
-	IsErr       string
+	IsErr       bool
 	Err         string
-	SubTests    *TestStat
+	Plans       []*PlanStat
+	SubTests    []*TestStat
+}
+
+func NewTestStat(id string, directory string, name string, description string) *TestStat {
+	return &TestStat{
+		ID:          id,
+		Directory:   directory,
+		Name:        name,
+		Description: description,
+	}
+}
+
+func (s *TestStat) SetError(err error) *TestStat {
+	s.IsErr = true
+	s.Err = err.Error()
+	return s
+}
+
+func (s *TestStat) AddPlanStat(plan *PlanStat) {
+	s.Plans = append(s.Plans, plan)
+}
+
+func (s *TestStat) AddSubTestStat(subTest *TestStat) {
+	s.SubTests = append(s.SubTests, subTest)
 }
